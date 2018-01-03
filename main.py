e512e07c49a4ac50ca1b44eb2c81f3b578f1c381
CBinyenya <calbinyex@gmail.com>
2018-01-03 15:22:29 +0300

    Changes

--- main.py
+++ main.py
@@ -16,13 +16,4 @@ def greeting():
 	return r.text
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',5000)
-=======
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
     app.run('0.0.0.0',8080)
