02284bd91f6271adaa415bcb03fe6cdd51c48072
CBinyenya <calbinyex@gmail.com>
2018-01-03 12:00:11 +0300

    updated funtionality

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import requests
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello World! from flask2'
+
+@app.route('/flask2')
+def flask1():
+	return 'I am flask 2'
+
+@app.route('/greeting')
+def greeting():
+	r = requests.get('http://flask1/hello')	
+	return r.text
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',5000)
